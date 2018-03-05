b7c59353238807212e4106147ef3d34a72778ccf
Brian Dawn <brian.t.dawn@gmail.com>
2018-03-05 13:29:25 -0600

    Add gtk support.

--- default.nix
+++ default.nix
@@ -2,6 +2,10 @@ with import <nixpkgs> {};
 
 let 
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/nixos-17.09.tar.gz") {};
+  opencv3 = pkgs.python36Packages.opencv3.override {
+    enableContrib   = true;
+    enableGtk2      = true;
+};
 in
 { nixpkgs ? pkgs }:
 stdenv.mkDerivation rec {
@@ -13,10 +17,10 @@ stdenv.mkDerivation rec {
 
 
     zlib
+    opencv3
 
     python3
     python36Packages.numpy
-    python36Packages.opencv3
     python36Packages.scipy
     python36Packages.matplotlib
     python36Packages.pylint
